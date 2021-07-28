3df0d568cd9595f24e0c6a767d7ebbef7798acb2
FreddyYJ <kyj1411@unist.ac.kr>
2021-07-28 11:50:01 +0900

    Improve afl plot script

--- tools/afl_plot.py
+++ tools/afl_plot.py
@@ -6,16 +6,19 @@ import matplotlib.pyplot as plt
 from matplotlib.transforms import Bbox
 
 
-def afl_plot(in_file: str, out_file: str, title: str, ignore_iteration: bool = False) -> None:
-    iter_list = list()
+def afl_plot(in_file: str, title: str, out_file: str='',ignore_iteration: bool = False,red="prophet",blue="random",green="guided") -> None:
+    prophet_iter_list = list()
+    random_iter_list = list()
+    guided_iter_list = list()
     prophet_result_list = list()
-    random_result_list=[]
-    guided_result_list=[]
+    random_result_list=list()
+    guided_result_list=list()
     index = 0
 
     max_success=0 # Just for graph design
-    with open(in_file+"/prophet.csv", "r") as csv:
+    with open(in_file+"/"+red+".csv", "r") as csv:
         total = 0
+        i=0
         for line in csv.readlines():
             tokens = line.strip().split(",")
             iter = int(tokens[0])
@@ -26,15 +29,17 @@ def afl_plot(in_file: str, out_file: str, title: str, ignore_iteration: bool = F
                 total += 1
                 max_success+=1
             if ignore_iteration:
-                iter_list.append(index)
+                prophet_iter_list.append(index)
                 index += 1
             else:
-                iter_list.append(iter)
+                prophet_iter_list.append(i)
+                i+=1
             prophet_result_list.append(total)
     
     temp_success=0
-    with open(in_file+"/random.csv", "r") as csv:
+    with open(in_file+"/"+blue+".csv", "r") as csv:
         total = 0
+        i=0
         for line in csv.readlines():
             tokens = line.strip().split(",")
             iter = int(tokens[0])
@@ -45,17 +50,19 @@ def afl_plot(in_file: str, out_file: str, title: str, ignore_iteration: bool = F
                 total += 1
                 temp_success+=1
             if ignore_iteration:
-                iter_list.append(index)
+                random_iter_list.append(index)
                 index += 1
             else:
-                iter_list.append(iter)
+                random_iter_list.append(i)
+                i+=1
             random_result_list.append(total)
     if max_success<temp_success:
         max_success=temp_success
 
-    max_success=0
-    with open(in_file+"/guided.csv", "r") as csv:
+    temp_success=0
+    with open(in_file+"/"+green+".csv", "r") as csv:
         total = 0
+        i=0
         for line in csv.readlines():
             tokens = line.strip().split(",")
             iter = int(tokens[0])
@@ -64,22 +71,23 @@ def afl_plot(in_file: str, out_file: str, title: str, ignore_iteration: bool = F
             result = tokens[3] == "1"                
             if result:
                 total += 1
-                max_success+=1
+                temp_success+=1
             if ignore_iteration:
-                iter_list.append(index)
+                guided_iter_list.append(index)
                 index += 1
             else:
-                iter_list.append(iter)
+                guided_iter_list.append(i)
+                i+=1
             guided_result_list.append(total)
     if max_success<temp_success:
-        max_success<temp_success
+        max_success=temp_success
 
     print(f"total {total}")
-    plt.plot(iter_list, prophet_result_list, 'r',iter_list,random_result_list,'b',iter_list,guided_result_list,'g')
+    plt.plot(prophet_iter_list, prophet_result_list, 'r',random_iter_list,random_result_list,'b',guided_iter_list,guided_result_list,'g')
     plt.title(title)
     plt.xlabel("iteration")
     plt.ylabel("pass")
-    plt.text(0,max_success,'Red: prophet\nBlue: random\nGreen: guided',Bbox={'facecolor':'white'})
+    plt.text(0,max_success,'Red: '+red+'\nBlue: '+blue+'\nGreen: '+green,bbox={'facecolor':'white'})
     plt.grid(True)
     if len(out_file) == 0:
         out_file = os.path.join(os.path.dirname(in_file), "plot.png")
@@ -102,7 +110,7 @@ def main(argv):
         elif o == "-n":
             ignore = True
 
-    afl_plot(result_file, output_file, title, ignore)
+    afl_plot(result_file, title, output_file, ignore)
     return 0
 
 
