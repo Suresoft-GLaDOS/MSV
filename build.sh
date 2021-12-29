eb12ce1d236121f1fdfc25bca883e1ba8d350a5f
FreddyYJ <kyj1411@unist.ac.kr>
2021-12-29 12:02:31 +0900

    Update build.sh for CPR

--- build.sh
+++ build.sh
@@ -7,4 +7,11 @@ automake --add-missing
 make -j 10 install
 cd AFLplusplus
 make -j 10 install
+
+# Generate llvm bit-code for runtime library, for symbolic execution
+cd ../src/.libs
+wllvm++ -g -fPIC -O0 -c -o _test_runtime.o ../_test_runtime.cpp
+llvm-ar rcs libtest_runtime.a _test_runtime.o
+extract-bc libtest_runtime.a
+cd ../..
 for i in `ipcs | grep $LOGNAME | awk '{print $2}'`; do ipcrm -m $i; done;
