f063a8c278420dc416fcb506043b6e8763ad3ee7
FreddyYJ <kyj1411@unist.ac.kr>
2022-03-15 17:37:07 +0900

    Fix libtiff test script

--- tools/libtiff-test.py
+++ tools/libtiff-test.py
@@ -186,7 +186,7 @@ def num2testcase( case ):
 import psutil
 
 def run_test(testcase,id,env,timeout):
-    proc = subprocess.Popen(["make", "check", "TESTS="+testcase], shell=True, env = env,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    proc = subprocess.Popen(["make", "check", "TESTS="+testcase], env = env,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     try:
         so,se=proc.communicate(timeout=timeout)
         if proc.returncode==0:
@@ -235,8 +235,6 @@ if __name__ == "__main__":
 
         chdir(cur_dir + "/"+temp_dir);
         system("rm -rf o-*.tiff o-*.ps o-*.pdf")
-        system('mv common.sh common-bak.sh')
-        system(f"cp -f {environ['MSV_PATH']}/tools/libtiff-common.sh ./common.sh")
 
         my_env = environ;
         my_env["GENEXPOUT"] = "0";
@@ -252,9 +250,7 @@ if __name__ == "__main__":
         pool.close()
         pool.join()
 
-        system('rm -f common.sh')
         system(f'killall --wait {cur_dir}/* > /dev/null 2>&1')
-        system('mv common-bak.sh common.sh')
         chdir(ori_dir);
         subprocess.call('rm -rf '+cur_dir+'/'+temp_dir,shell=True)
 
