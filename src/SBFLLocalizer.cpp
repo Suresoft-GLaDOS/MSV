d7cd08b328a6421c721e09e68980d7c04b02c782
FreddyYJ <kyj1411@unist.ac.kr>
2022-03-30 11:50:53 +0900

    Update SBFL Reader

--- src/SBFLLocalizer.cpp
+++ src/SBFLLocalizer.cpp
@@ -1,6 +1,7 @@
 #include <clang/AST/AST.h>
 #include <clang/AST/RecursiveASTVisitor.h>
 #include <clang/AST/Stmt.h>
+#include "cJSON/cJSON.h"
 #include "SBFLLocalizer.h"
 
 using namespace clang;
@@ -35,3 +36,44 @@ public:
         return true;
     }
 };
+
+std::vector<SourcePositionTy> SBFLLocalizer::getCandidateLocations() {
+    // Get sbfl result
+    std::ifstream file(SBFLFile);
+    std::string rootString="";
+    std::string line;
+    while (std::getline(file,line)){
+        rootString+=line+"\n";
+    }
+    file.close();
+
+    std::map<std::string,std::map<unsigned int,double>> sbflResult;
+    sbflResult.clear();
+    cJSON *root=cJSON_Parse(rootString.c_str());
+    cJSON *location=NULL;
+    cJSON_ArrayForEach(location,root){
+        cJSON *file_elem=cJSON_GetArrayItem(location,0);
+        std::string file_name(cJSON_GetStringValue(file_elem));
+        unsigned int line=(unsigned int)cJSON_GetNumberValue(cJSON_GetArrayItem(location,1));
+        double score=cJSON_GetNumberValue(cJSON_GetArrayItem(location,2));
+
+        if (sbflResult.count(file_name)==0)
+            sbflResult[file_name]=std::map<unsigned int,double>();
+        
+        sbflResult[file_name][line]=score;
+    }
+
+    // Ge Profile location from sbfl
+    for (std::map<std::string,std::map<unsigned int,double>>::iterator it=sbflResult.begin();it!=sbflResult.end();it++){
+        std::string fullPath=program->getSrcdir()+"/"+it->first;
+        std::string code;
+        readCodeToString(fullPath,code);
+        std::unique_ptr<ASTUnit> unit=program->buildClangASTUnit(fullPath,code);
+        
+        ASTContext &ctxt=unit->getASTContext();
+        ProfileLocationParser parser(it->second,&ctxt,it->first);
+        parser.TraverseTranslationUnitDecl(ctxt.getTranslationUnitDecl());
+
+        std::vector<std::pair<SourcePositionTy,double>> result=parser.getResult();
+    }
+}
