7eb3caf4f605a2ad17a4086e2a345a6ede9305a7
FreddyYJ <kyj1411@unist.ac.kr>
2022-04-04 18:03:25 +0900

    Fix new php tester

--- tools/new-php-test.py
+++ tools/new-php-test.py
@@ -91,6 +91,7 @@ if __name__ == "__main__":
 
         ori_dir = getcwd()
         chdir(work_dir)
+        subprocess.run(['sed','-i','s|/usr/bin/php|./sapi/cli/php|g','php-run-tests.c'])
         subprocess.run(['gcc','-o','php-run-tests','php-run-tests.c'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         chdir(cur_dir)
         result=[]
