1e8a17a2c7b9654d9bd4e7e9fd53ad9b9f54a747
FreddyYJ <kyj1411@unist.ac.kr>
2022-04-08 14:06:57 +0900

    Remove CPR stuffs in build.sh

--- build.sh
+++ build.sh
@@ -5,14 +5,3 @@ autoconf
 automake --add-missing
 ./configure
 make -j 10 install
-
-export LLVM_COMPILER=clang
-export LLVM_CC_NAME=clang
-export LLVM_CXX_NAME=clang++
-# Generate llvm bit-code for runtime library, for symbolic execution
-cd src/.libs
-wllvm++ -g -fPIC -O0 -c -o _test_runtime.o ../_test_runtime.cpp
-llvm-ar rcs libtest_runtime.a _test_runtime.o
-extract-bc libtest_runtime.a
-cd ../..
-for i in `ipcs | grep $LOGNAME | awk '{print $2}'`; do ipcrm -m $i; done;
