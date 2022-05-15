904d75b2e81483676f0c16b6b283f4c85d83ec73
FreddyYJ <kyj1411@unist.ac.kr>
2022-05-15 10:31:01 +0900

    Add skip files for sbfl

--- src/SBFLLocalizer.cpp
+++ src/SBFLLocalizer.cpp
@@ -81,7 +81,7 @@ public:
 };
 
 bool skipFile(std::string &fileName){
-    return fileName=="crypto/des/ncbc_enc.c" || fileName=="providers/implementations/ciphers/cipher_aes_cts.inc";
+    return fileName=="crypto/des/ncbc_enc.c" || fileName.find(".inc")!=std::string::npos;
 }
 
 SBFLLocalizer::SBFLLocalizer(std::string fileName,BenchProgram *program): SBFLFile(fileName),program(program),result() {
