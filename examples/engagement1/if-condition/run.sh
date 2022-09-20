c98eb0d2c136007a2160ff5725cb668062b55d20
FreddyYJ <kyj1411@unist.ac.kr>
2022-09-20 15:30:51 +0900

    Update if condition example script

--- examples/engagement1/if-condition/run.sh
+++ examples/engagement1/if-condition/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm __fixed_*
-prophet run.conf  -r . -consider-all -first-n-loc 200 -feature-para ../../../crawler/para-all.out
-
+# ../../../build/src/prophet run.conf -consider-all -first-n-loc 200 -feature-para ../../../crawler/para-all.out
+prophet -r . -consider-all -first-n-loc 200 -feature-para ../../../crawler/para-rext-all.out -skip-verify -skip-profile -replace-ext -msv-ext
+cat fixed_test.c
