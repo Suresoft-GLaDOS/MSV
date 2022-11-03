59511194573fc10bf694b06184fd1c1935dcbadc
FreddyYJ <kyj1411@unist.ac.kr>
2022-11-03 14:42:36 +0900

    Update runtime library

--- src/_test_runtime.cpp
+++ src/_test_runtime.cpp
@@ -33,10 +33,6 @@
 #include <time.h>
 #include <cstdint>
 
-#include <limits.h>
-#include <dirent.h>
-#include <stdbool.h>
-
 #define MAXSZ 1048576
 #define ONE_OR_N_BIT 50
 
@@ -124,61 +120,21 @@ extern "C" int __choose(const char *switch_id) {
 
 #define MAGIC_NUMBER -123456789
 
-extern "C" char *__stat_write_init(const char *func_name){
+extern "C" void __write_profile(const char *func_name,int mode,const char *var_name,void *var_addr,int size){
     char * pid = getenv("__PID");
     if (pid == NULL || strlen(pid) == 0) {
-        return NULL;
+        pid = "0";
     }
+    char tmp_file[200];
+    sprintf(tmp_file,"/tmp/%s_%s_profile.log",pid,func_name);
 
-    char *tmp_dir = getenv("MSV_TMP_DIR");
-    if (tmp_dir == NULL || strlen(tmp_dir) == 0) {
-        tmp_dir = "/tmp";
-    }
-
-    char log_file[1024];
-    sprintf(log_file,"%s/%s_profile.log", tmp_dir, pid);
-    // fprintf(stderr, "good pid\n");
-    
-    int included=0;
-    FILE *log_r=fopen(log_file,"r");
-    if (log_r==NULL)
-        log_r=fopen(log_file,"w");
-    else{
-        char *line=NULL;
-        size_t length=0;
-        // fprintf(stderr, "before getline\n");
-        while (getline(&line,&length,log_r)!=-1){
-            line[strlen(line)-1]='\0';
-            if (strcmp(line,func_name)==0){
-                included=1;
-                break;
-            }
-        }
-        // fprintf(stderr, "after getline\n");
-        free(line);
-    }
-    fclose(log_r);
-
-    if (!included){
-        FILE *log=fopen(log_file,"a");
-        fprintf(log,"%s\n",func_name);
-        fclose(log);
-    }
-    if (getenv("MSV_RUN_ORIGINAL")!=NULL){
-        return NULL;
-    }
-
-    char *str=(char *)malloc(sizeof(char)*1000000);
-    sprintf(str,"");
-    return str;
-}
-extern "C" void __write_stat(char *str,const char *var_name,void *var_addr,int size){
-    char * pid = getenv("__PID");
-    if (pid == NULL || strlen(pid) == 0 || getenv("MSV_RUN_ORIGINAL") != NULL) {
-        return;
-    }
-
-    if (str==NULL) return;
+    // fprintf(stderr, "\n%s: %d\n", func_name,count);
+    FILE *f;
+    if (mode==0)
+        f = fopen(tmp_file, "w");
+    else
+        f = fopen(tmp_file, "a");
+    // assert( sz <= 8 );
     long long v = 0;
     if (size<=8 && isGoodAddr(var_addr, size)) {
         memcpy(&v, var_addr, size);
@@ -186,34 +142,46 @@ extern "C" void __write_stat(char *str,const char *var_name,void *var_addr,int s
     else {
         v = MAGIC_NUMBER;
     }
-    sprintf(str, "%s%s=%lld\n", str,var_name,v);
+    fprintf(f, "%s=%lld\n", var_name,v);
     // fprintf(stderr, "%s=%lld\n", name, v);
+    fclose(f);
+
+    if (mode==0){
+        int runned=0;
+        char log_file[1024];
+        sprintf(log_file,"/tmp/%s_profile.log",pid);
+        // fprintf(stderr, "good pid\n");
+        
+        int included=0;
+        FILE *log_r=fopen(log_file,"r");
+        if (log_r==NULL)
+            log_r=fopen(log_file,"w");
+        else{
+            char *line=NULL;
+            size_t length=0;
+            // fprintf(stderr, "before getline\n");
+            while (getline(&line,&length,log_r)!=-1){
+                line[strlen(line)-1]='\0';
+                if (strcmp(line,func_name)==0){
+                    included=1;
+                    break;
+                }
+            }
+            // fprintf(stderr, "after getline\n");
+            free(line);
+        }
+        fclose(log_r);
+
+        if (!included){
+            FILE *log=fopen(log_file,"a");
+            fprintf(log,"%s\n",func_name);
+            fclose(log);
+        }
+    }
     // fprintf(stderr, "exit\n");
 }
 
-extern "C" void __stat_file_close(const char *func_name,char *str){
-    char * pid = getenv("__PID");
-    if (pid == NULL || strlen(pid) == 0 || getenv("MSV_RUN_ORIGINAL") != NULL) {
-        return;
-    }
-
-    if (str==NULL) return;
-    char *tmp_dir = getenv("MSV_TMP_DIR");
-    if (tmp_dir == NULL || strlen(tmp_dir) == 0) {
-        tmp_dir = "/tmp";
-    }
-
-    char tmp_file[1024];
-    sprintf(tmp_file, "%s/%s_%s_profile.log", tmp_dir, pid, func_name);
-
-    FILE *f;
-    f = fopen(tmp_file, "w");
-    fprintf(f,str);
-    fclose(f);
-    free(str);
-}
-
-extern "C" int __is_neg(const char *location,char *lid,int count, ...) {
+extern "C" int __is_neg(int count, ...) {
     // fprintf(stderr, "fuck\n");
     if (!enable) return 0;
     char* is_neg = getenv("IS_NEG");
@@ -397,58 +365,20 @@ extern "C" int __is_neg(const char *location,char *lid,int count, ...) {
             void* p = va_arg(ap, void*);
             unsigned long sz = va_arg(ap, unsigned long);
             assert( sz <= 8 );
-
-            // We assume that all variables are signed
-            // TODO: handle unsigned variables
-            if (sz==8){
-                int64_t v = 0;
-                if (isGoodAddr(p, sz)) {
-                    memcpy(&v, p, sz);
-                }
-                else {
-                    v = MAGIC_NUMBER;
-                }
-                fprintf(f, " %lld", v);
+            long long v = 0;
+            if (isGoodAddr(p, sz)) {
+                memcpy(&v, p, sz);
             }
-            else if (sz==4){
-                int32_t v = 0;
-                if (isGoodAddr(p, sz)) {
-                    memcpy(&v, p, sz);
-                }
-                else {
-                    v = MAGIC_NUMBER;
-                }
-                fprintf(f, " %d", v);
+            else {
+                v = MAGIC_NUMBER;
             }
-            else if (sz==2){
-                int16_t v = 0;
-                if (isGoodAddr(p, sz)) {
-                    memcpy(&v, p, sz);
-                }
-                else {
-                    v = MAGIC_NUMBER;
-                }
-                fprintf(f, " %d", v);
-            }
-            else if (sz==1){
-                int8_t v = 0;
-                if (isGoodAddr(p, sz)) {
-                    memcpy(&v, p, sz);
-                }
-                else {
-                    v = MAGIC_NUMBER;
-                }
-                fprintf(f, " %d", v);
-            }
+            fprintf(f, " %lld", v);
+            // fprintf(stderr, "i %lu %lld\n", i, v);
         }
         fprintf(f, "\n");
         fclose(f);
 
         if (strcmp(is_neg, "RECORD1") == 0) {
-            if (current_cnt>=records_sz){
-                records[current_cnt]=0;
-                records_sz++;
-            }
             // We write back with additional int to note the end
             char* neg_arg = getenv("NEG_ARG");
             f = fopen(neg_arg, "w");
@@ -461,11 +391,11 @@ extern "C" int __is_neg(const char *location,char *lid,int count, ...) {
                 if (i != records_sz - 1)
                     fprintf(f, " ");
             }
-            // fprintf(f, "%lu", current_cnt + 1);
+            fprintf(f, "%lu", current_cnt + 1);
             // fprintf(stderr, "count: %d\n",current_cnt+1);
             fclose(f);
 
-            // assert( current_cnt < records_sz);
+            assert( current_cnt < records_sz);
             //fprintf(stderr, "fuck you %lu\n", records[current_cnt]);
             return records[current_cnt++];
         }
@@ -473,35 +403,63 @@ extern "C" int __is_neg(const char *location,char *lid,int count, ...) {
             return 0;
     }
     else if (strcmp(is_neg, "RUN") == 0){
-        char temp[20];
-        strcpy(temp,location);
-        char temp_location[40]="__";
-        char *temp2;
-        temp2=strtok(temp,"-");
-        strcat(temp_location,temp2);
-        strcat(temp_location,"_");
-        strcat(temp_location,strtok(NULL,"-"));
-
         // If operator is ALL_1, return 1
-        char operator_temp[60];
-        strcpy(operator_temp,temp_location);
-        strcat(operator_temp,"__OPERATOR");
-        char variable_temp[60];
-        strcpy(variable_temp,temp_location);
-        strcat(variable_temp,"__VARIABLE");
-        char constant_temp[60];
-        strcpy(constant_temp,temp_location);
-        strcat(constant_temp,"__CONSTANT");
+        char* tmp_file = getenv("TMP_FILE");
+        if (!init) {
+            // fprintf(stderr,"Initing 1!\n");
+            init = true;
+            FILE *f = fopen(tmp_file, "r");
+            if (f == NULL) {
+                records_sz = 0;
+                current_cnt = 0;
+            }
+            else {
+                unsigned long n;
+                int ret = fscanf(f, "%lu", &n);
+                assert( ret == 1);
+                records_sz = 0;
+                for (unsigned long i = 0; i < n; i++) {
+                    unsigned long v;
+                    ret = fscanf(f, "%lu", &v);
+                    assert( ret == 1);
+                    records[records_sz ++ ] = v;
+                }
+                fclose(f);
+                current_cnt = records_sz;
+            }
+        }
+        
 
-        if (strcmp(getenv(operator_temp),"4")==0) {
+        if (strcmp(getenv("__OPERATOR"),"4")==0) {
+            if (records_sz < MAXSZ){
+                records[records_sz++] = 1;
+            }
+            current_cnt ++;
+
+            // fprintf(stderr,"Current cnt, Record size: %d %d\n",current_cnt,records_sz);
+            // We write back immediate
+            FILE *f = fopen(tmp_file, "w");
+            assert( f != NULL );
+            fprintf(f, "%lu ", records_sz);
+            // fprintf(stderr, "Size: %lu\n",records_sz);
+            // fprintf(stderr, "Record: ");
+            for (unsigned long i = 0; i < records_sz; i++) {
+                fprintf(f, "%lu", records[i]);
+                // fprintf(stderr, "%lu ",records[i]);
+                if (i != records_sz - 1)
+                    fprintf(f, " ");
+            }
+            // fprintf(stderr, "\n");
+            fclose(f);
+
+
             return 1;
         }
 
-        int var=atoi(getenv(variable_temp));
-        int oper=atoi(getenv(operator_temp));
-        long long constant=(long long)atoi(getenv(constant_temp));
+        int var=atoi(getenv("__VARIABLE"));
+        int oper=atoi(getenv("__OPERATOR"));
+        int constant=atoi(getenv("__CONSTANT"));
         long long value=0;
-        long long value2=0;
 
         va_list ap;
         va_start(ap, count);
@@ -511,131 +469,58 @@ extern "C" int __is_neg(const char *location,char *lid,int count, ...) {
             assert( sz <= 8 );
 
             if (i==var){
-                // We assume that all variables are signed
-                // TODO: handle unsigned variables
-                if (sz==8){
-                    int64_t v = 0;
-                    if (isGoodAddr(p, sz)) {
-                        memcpy(&v, p, sz);
-                    }
-                    else {
-                        v = MAGIC_NUMBER;
-                    }
-                    value=(long long) v;
+                if (isGoodAddr(p, sz)) {
+                    memcpy(&value, p, sz);
                 }
-                else if (sz==4){
-                    int32_t v = 0;
-                    if (isGoodAddr(p, sz)) {
-                        memcpy(&v, p, sz);
-                    }
-                    else {
-                        v = MAGIC_NUMBER;
-                    }
-                    value=(long long) v;
+                else {
+                    value = MAGIC_NUMBER;
                 }
-                else if (sz==2){
-                    int16_t v = 0;
-                    if (isGoodAddr(p, sz)) {
-                        memcpy(&v, p, sz);
-                    }
-                    else {
-                        v = MAGIC_NUMBER;
-                    }
-                    value=(long long) v;
-                }
-                else if (sz==1){
-                    int8_t v = 0;
-                    if (isGoodAddr(p, sz)) {
-                        memcpy(&v, p, sz);
-                    }
-                    else {
-                        v = MAGIC_NUMBER;
-                    }
-                    value=(long long) v;
-                }
-
-                if (oper<=4)
-                    break;
-            }
-
-            else if (oper>=5 && i==constant){
-                // We assume that all variables are signed
-                // TODO: handle unsigned variables
-                if (sz==8){
-                    int64_t v = 0;
-                    if (isGoodAddr(p, sz)) {
-                        memcpy(&v, p, sz);
-                    }
-                    else {
-                        v = MAGIC_NUMBER;
-                    }
-                    value2=(long long) v;
-                }
-                else if (sz==4){
-                    int32_t v = 0;
-                    if (isGoodAddr(p, sz)) {
-                        memcpy(&v, p, sz);
-                    }
-                    else {
-                        v = MAGIC_NUMBER;
-                    }
-                    value2=(long long) v;
-                }
-                else if (sz==2){
-                    int16_t v = 0;
-                    if (isGoodAddr(p, sz)) {
-                        memcpy(&v, p, sz);
-                    }
-                    else {
-                        v = MAGIC_NUMBER;
-                    }
-                    value2=(long long) v;
-                }
-                else if (sz==1){
-                    int8_t v = 0;
-                    if (isGoodAddr(p, sz)) {
-                        memcpy(&v, p, sz);
-                    }
-                    else {
-                        v = MAGIC_NUMBER;
-                    }
-                    value2=(long long) v;
-                }
+                break;
             }
         }
 
-        int result;
         if (value==MAGIC_NUMBER) return 0;
         else{
             switch(oper){
                 case 1: 
-                    result = (value !=constant);
-                    break;
+                    if (records_sz < MAXSZ){
+                        records[records_sz++] = (value !=constant);
+                    }
                 case 2: 
-                    result = (value >constant);
-                    break;
+                    if (records_sz < MAXSZ){
+                        records[records_sz++] = (value >constant);
+                    }
                 case 3: 
-                    result = (value <constant);
-                    break;
-                case 5:
-                    result = (value ==value2);
-                    break;
-                case 6:
-                    result=(value!=value2);
-                    break;
-                case 7:
-                    result=(value>value2);
-                    break;
-                case 8:
-                    result=(value<value2);
-                    break;
+                    if (records_sz < MAXSZ){
+                        records[records_sz++] = (value <constant);
+                    }
                 default: 
-                    result = (value ==constant);
-                    break;
+                    if (records_sz < MAXSZ){
+                        records[records_sz++] = (value ==constant);
+                    }
             }
 
-            return result;
+            current_cnt ++;
+
+            // fprintf(stderr,"Current cnt, Record size: %d %d\n",current_cnt,records_sz);
+            // We write back immediate
+            FILE *f = fopen(tmp_file, "w");
+            assert( f != NULL );
+            fprintf(f, "%lu ", records_sz);
+            // fprintf(stderr, "Size: %lu\n",records_sz);
+            // fprintf(stderr, "Record: ");
+            for (unsigned long i = 0; i < records_sz; i++) {
+                fprintf(f, "%lu", records[i]);
+                // fprintf(stderr, "%lu ",records[i]);
+                if (i != records_sz - 1)
+                    fprintf(f, " ");
+            }
+            // fprintf(stderr, "\n");
+            fclose(f);
+
+            return records[records_sz-1];
         }
+
     }
     return 0;
 }
@@ -650,40 +535,5 @@ extern "C" long long __mutate(const long long value,const char *oper_env,const c
         case 3: return value * constant; // mult
         case 4: return value / constant; // div
         default: return constant; // assign (default)
-    }
-}
-
-extern "C" void *__var_select(unsigned int var_count,void *vars[]){
-    char *var=getenv("__SELECT_VAR");
-    if (var==NULL) return vars[0];
-    else{
-        int index=atoi(var);
-        return vars[index];
-    }
-}
-
-extern "C" long long __const_select(unsigned int const_count, ...){
-    char *var=getenv("__SELECT_VAR");
-    va_list ap;
-    va_start(ap,const_count);
-
-    if (var==NULL) return 0;
-    else{
-        int index=atoi(var);
-        long long result=0;
-        for (int i=0;i<index;i++){
-            va_arg(ap,long long);
-        }
-        result=va_arg(ap,long long);
-        return result;
-    }
-}
-
-extern "C" void *__var_select_2(unsigned int var_count,void *vars[]){
-    char *var=getenv("__SELECT_VAR_2");
-    if (var==NULL) return vars[0];
-    else{
-        int index=atoi(var);
-        return vars[index];
     }
 }
